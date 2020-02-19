704cfc99166abd644a51acd68573694c1af9f6cb
Heber <hebergonza@gmail.com>
2020-02-19 01:55:49 -0500

    finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import axios from "axios";
+
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -10,6 +13,21 @@ const App = () => {
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+
+  const [movieList, setMovieList] = useState([]);
+
+  const fetchData = () => {
+    axios
+    .get("http://localhost:5000/api/movies")
+    .then(res => {
+      setMovieList(res.data);
+      })
+    .catch(err => console.log(err.response));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -19,6 +37,12 @@ const App = () => {
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
+        }}
+      />
+      <Route
+        path="/update-movie/:id"
+        render = {props => {
+          return <UpdateMovie {...props} fetchData={fetchData} movieList={movieList} updateList={setMovieList}/>;
         }}
       />
     </>
